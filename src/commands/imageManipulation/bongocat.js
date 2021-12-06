3c49588b6e5e96f0e7102eaf22c3f92644ca7007
GREENMINDED01 <79315838+GREENMINDED01@users.noreply.github.com>
2021-12-06 22:11:06 +0800

    Delete src/commands/imageManipulation directory

--- src/commands/imageManipulation/bongocat.js
+++
@@ -1,6 +0,0 @@
-const GenericImageCommand = require('../../models/GenericImageCommand.js');
-
-module.exports = new GenericImageCommand({
-  triggers: ['bongocat', 'bongo'],
-  description: 'smack smack...'
-});
