94b516adbb62e44f5ef545fc272d34d138dd4963
GREENMINDED01 <79315838+GREENMINDED01@users.noreply.github.com>
2021-12-06 22:09:41 +0800

    Delete src/commands/soundCommands directory

--- src/commands/soundCommands/playclip.js
+++
@@ -1,9 +0,0 @@
-const GenericVoiceCommand = require('../../models/GenericVoiceCommand');
-
-module.exports = new GenericVoiceCommand({
-  triggers: ['playclip'],
-  description: 'Plays a custom sound clip',
-  usage: '{command} <clipname>',
-  existingConn: 'Develop the technology to speak two different things simultaneously and then we\'ll talk.',
-  ownerOnly: true
-});
